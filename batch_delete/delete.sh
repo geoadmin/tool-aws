816d10f82cb8ab7c871c5998fe42a17eafa90805
Gilbert Jeiziner <gjn@space.ch>
2017-04-06 13:09:44 +0200

    Adding a little security by checking length of passed prefix

--- batch_delete/delete.sh
+++ batch_delete/delete.sh
@@ -2,7 +2,13 @@
 
 for line in `cat $1`
 do
-  echo '1.0.0/'$line'/*'
-  s3rm -f --profile ltjeg_aws_s3_tiles --bucket-name akiai4jxkwjqv5tgsaoq-wmts --prefix '1.0.0/'$line'/*'
+  prefix='1.0.0/'$line'/*'
+  size=${#prefix}
+  echo $prefix' ('$size')'
+  if [ "$size" -gt 17 ]; then
+    s3rm -f --profile ltjeg_aws_s3_tiles --bucket-name akiai4jxkwjqv5tgsaoq-wmts --prefix $prefix
+  else
+    echo 'ignored because prefix is too small '$prefix
+  fi
 done
 
