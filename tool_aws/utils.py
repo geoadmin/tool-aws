16667bd651981162f47bde3dbf28bd2108a6900a
loicgasser <loicgasser4@gmail.com>
2017-11-30 09:27:48 +0100

    Consistent camelCase

--- tool_aws/utils.py
+++ tool_aws/utils.py
@@ -1,11 +1,11 @@
 from pyproj import Proj, transform
 
 
-def reprojectBBox(bbox, srid_to, srid_from=2056):
-    if srid_to == srid_from:
+def reprojectBBox(bbox, sridTo, sridFrom=2056):
+    if sridTo == sridFrom:
         return bbox
-    srid_in = Proj('+init=EPSG:%s' % srid_from)
-    srid_out = Proj('+init=EPSG:%s' % srid_to)
-    p_left = transform(srid_in, srid_out, bbox[0], bbox[1])
-    p_right = transform(srid_in, srid_out, bbox[2], bbox[3])
-    return p_left + p_right
+    sridIn = Proj('+init=EPSG:%s' % sridFrom)
+    srid_out = Proj('+init=EPSG:%s' % sridTo)
+    pLeft = transform(sridIn, srid_out, bbox[0], bbox[1])
+    pRight = transform(sridIn, srid_out, bbox[2], bbox[3])
+    return pLeft + pRight
