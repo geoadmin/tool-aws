879f878b168951f2fb05d0f19f77813f75c26e8e
loicgasser <loicgasser4@gmail.com>
2017-12-07 13:32:27 +0100

    Fix style

--- tool_aws/utils.py
+++ tool_aws/utils.py
@@ -5,7 +5,7 @@ def reprojectBBox(bbox, sridTo, sridFrom=2056):
     if sridTo == sridFrom:
         return bbox
     sridIn = Proj('+init=EPSG:%s' % sridFrom)
-    srid_out = Proj('+init=EPSG:%s' % sridTo)
-    pLeft = transform(sridIn, srid_out, bbox[0], bbox[1])
-    pRight = transform(sridIn, srid_out, bbox[2], bbox[3])
+    sridOut = Proj('+init=EPSG:%s' % sridTo)
+    pLeft = transform(sridIn, sridOut, bbox[0], bbox[1])
+    pRight = transform(sridIn, sridOut, bbox[2], bbox[3])
     return pLeft + pRight
