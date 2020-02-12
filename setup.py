e98857887a0a0e2c77237163300855db6e15909c
Marc Mönnerat <procrastinatio@gmail.com>
2020-02-12 16:37:13 +0100

    Bumping version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 
 setup(name=u'tool_aws',
-      version=u'0.2.1',
+      version=u'0.2.2',
       description=u'AWS scripts for geoadmin',
       author=u'Andrea Borghi, Loic Gasser',
       author_email=u'andrea.borghi@swisstopo.ch, loicgasser4@gmail.com',
