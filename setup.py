7e01902ae95d0b530427025e0b9411404d6255ee
Marc Mönnerat <procrastinatio@gmail.com>
2020-02-17 06:49:23 +0100

    Support for python3.8 (#24)
    * testing for python3.8
    
    * python3.8

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(name=u'tool_aws',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Scientific/Engineering :: GIS',
           'Topic :: Software Development :: Libraries :: Python Modules'
       ],
