c0a38d44e20b3c41d717c12d4eae32feed336fe2
Tobias Reber <tobias.reber@swisstopo.ch>
2022-09-15 14:02:10 +0200

    BGDIINF_SB-2209: Proj definition changed due to update of proj

--- tool_aws/utils.py
+++ tool_aws/utils.py
@@ -4,8 +4,8 @@ from pyproj import Proj, transform
 def reprojectBBox(bbox, sridTo, sridFrom=2056):
     if sridTo == sridFrom:
         return bbox
-    sridIn = Proj('+init=EPSG:%s' % sridFrom)
-    sridOut = Proj('+init=EPSG:%s' % sridTo)
+    sridIn = Proj('EPSG:%s' % sridFrom)
+    sridOut = Proj('EPSG:%s' % sridTo)
     pLeft = transform(sridIn, sridOut, bbox[0], bbox[1])
     pRight = transform(sridIn, sridOut, bbox[2], bbox[3])
     return pLeft + pRight
